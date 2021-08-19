3433edd2d9b1bb111f75b9b36e0504de23a939a3
David Moles <dmoles@dmoles.net>
2021-08-19 12:16:32 -0700

    Fix collect.rb to load classes with Zeitwerk

--- bin/lending/collect.rb
+++ bin/lending/collect.rb
@@ -7,10 +7,31 @@ $stderr.sync = true
 # Require gems
 require 'bundler/setup'
 
-# Require lib/lending
-unless $LOAD_PATH.include?((lib_path = File.expand_path('../../lib', __dir__)))
-  puts "Adding #{lib_path} to $LOAD_PATH"
-  $LOAD_PATH.unshift(lib_path)
+# ------------------------------------------------------------
+# Make classes in app/lib available
+require "zeitwerk"
+
+module UCBEARS
+  # TODO: Share code with config/initializers/inflections.rb?
+  class CollectorInflector < Zeitwerk::Inflector
+    ACRONYMS = %w[IIIF UCBLIT UCBEARS].freeze
+
+    def camelize(basename, _abspath)
+      overrides[basename] || basename.split('_').map { |s| capitalize(s) }.join
+    end
+
+    def capitalize(s)
+      s.upcase.tap { |s_upcased| return s_upcased if ACRONYMS.include?(s_upcased) }
+      s.capitalize
+    end
+  end
+end
+
+Zeitwerk::Loader.new.tap do |loader|
+  loader.inflector = UCBEARS::CollectorInflector.new
+  lib_path = File.expand_path('../../app/lib', __dir__)
+  loader.push_dir(lib_path)
+  loader.setup
 end
 
 # Run collector
