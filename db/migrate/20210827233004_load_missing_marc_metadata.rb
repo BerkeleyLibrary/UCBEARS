b54a3fad99b92442e07176ff0d460259167fca8e
David Moles <dmoles@berkeley.edu>
2021-08-27 16:35:36 -0700

    Load MARC metadata for items that missed it, just this once

---
+++ db/migrate/20210827233004_load_missing_marc_metadata.rb
@@ -0,0 +1,10 @@
+# Ensure migration can run without error even if we delete/rename the models
+class LendingItemLoan < ActiveRecord::Base; end unless defined?(LendingItem)
+
+class LoadMissingMarcMetadata < ActiveRecord::Migration[6.0]
+  def change
+    LendingItem.where(publisher: nil).or(LendingItem.where(physical_desc: nil)).find_each do |item|
+      item.refresh_marc_metadata!
+    end
+  end
+end
