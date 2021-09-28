886438d912f1a8bf0e42569151d6389f1a5db2dc
David Moles <dmoles@berkeley.edu>
2021-09-28 13:17:08 -0700

    Stop VIPS operation caching

--- bin/lending/collect.rb
+++ bin/lending/collect.rb
@@ -14,6 +14,8 @@ require_relative '_zeitwerk'
 # Debug VIPS memory leaks, maybe
 require 'vips'
 Vips.leak_set(true)
+# Reduce VIPS memory usage? https://github.com/libvips/ruby-vips/issues/67#issuecomment-670201877
+Vips::cache_set_max(0)
 
 # Run collector
 collector = Lending::Collector.from_environment
