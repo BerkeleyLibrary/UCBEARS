1ed7b473badc27a59b752690c8e9b595d7eb4a3c
David Moles <dmoles@berkeley.edu>
2021-09-30 14:10:21 -0700

    Fix responsive hamburger focus appearance

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,16 @@
 import Rails from '@rails/ujs'
 
 Rails.start()
+
+window.addEventListener('load', () => {
+  const navMenuCheckbox = document.getElementById('nav-menu')
+  if (!navMenuCheckbox) {
+    console.log('navMenuCheckbox not found')
+    return
+  }
+  navMenuCheckbox.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') {
+      navMenuCheckbox.checked = !navMenuCheckbox.checked
+    }
+  })
+})
