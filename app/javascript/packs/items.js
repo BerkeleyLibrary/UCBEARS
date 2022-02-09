b41c6b4f77b666c5651e1b063842ffb32410132d
David Moles <dmoles@dmoles.net>
2022-02-09 14:49:42 -0800

    Only load Vue components when target div is present

--- app/javascript/packs/items.js
+++ app/javascript/packs/items.js
@@ -4,6 +4,11 @@ import ItemsAdmin from './items/components/ItemsAdmin.vue'
 import axios from 'axios'
 
 document.addEventListener('DOMContentLoaded', () => {
+  // TODO: find a more elegant way to only load this pack conditionally
+  if (!document.getElementById('items-table')) {
+    return
+  }
+
   const csrfToken = document.querySelector('meta[name="csrf-token"]')
   if (csrfToken) { // this won't be present in test
     axios.defaults.headers.common['X-CSRF-Token'] = csrfToken.getAttribute('content')
--- app/javascript/packs/terms.js
+++ app/javascript/packs/terms.js
@@ -4,6 +4,11 @@ import TermsAdmin from './terms/components/TermsAdmin.vue'
 import axios from 'axios'
 
 document.addEventListener('DOMContentLoaded', () => {
+  // TODO: find a more elegant way to only load this pack conditionally
+  if (!document.getElementById('terms-table')) {
+    return
+  }
+
   const csrfToken = document.querySelector('meta[name="csrf-token"]')
   if (csrfToken) { // this won't be present in test
     axios.defaults.headers.common['X-CSRF-Token'] = csrfToken.getAttribute('content')
