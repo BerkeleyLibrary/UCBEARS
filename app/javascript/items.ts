50fbfe901fb7dbe56df3672b791f7f2c6f30848e
David Moles <dmoles@berkeley.edu>
2023-06-15 14:51:36 -0700

    Use Vue3 components for items and terms

---
+++ app/javascript/items.ts
@@ -0,0 +1,4 @@
+import ItemsAdmin from "./components/ItemsAdmin.vue"
+import { onLoadMount } from "./helpers/init-helper";
+
+onLoadMount(ItemsAdmin, '#items-table')
---
+++ app/javascript/terms.ts
@@ -0,0 +1,4 @@
+import TermsAdmin from "./components/TermsAdmin.vue"
+import { onLoadMount } from "./helpers/init-helper";
+
+onLoadMount(TermsAdmin, '#terms-table')
