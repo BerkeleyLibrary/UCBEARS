23cd73caf6743ee17f9aebf2de78ee0100ea9ceb
Steve Sullivan <steve.sullivan@berkeley.edu>
2025-11-21 13:13:36 -0800

    ADA-325 add aria required to term start date field

--- app/javascript/terms/components/AddTermWidget.vue
+++ app/javascript/terms/components/AddTermWidget.vue
@@ -3,7 +3,7 @@
     <tr v-if="term" id="new-term-row" class="add-term">
       <td class="control"><input id="new-term-default_term" v-model.lazy="term.default_term" type="checkbox"></td>
       <td><input id="new-term-name" v-model.lazy="term.name" type="text" aria-required="true"></td>
-      <td><input id="new-term-start-date" v-model.lazy="term.start_date" type="date"></td>
+      <td><input id="new-term-start-date" v-model.lazy="term.start_date" type="date" aria-required="true"></td>
       <td><input id="new-term-end-date" v-model.lazy="term.end_date" type="date"></td>
       <td colspan="3">
         <div class="actions">
